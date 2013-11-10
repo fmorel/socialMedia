b6ac7cdf4e3abaa0000bd8ea4128cba7b4048b84
François-Xavier MOREL <efix.morel@gmail.com>
2013-11-10 23:24:10 +0100

    Update twitterInfo to bea abme to parse differnet kind of informations

--- twitterInfo.py
+++ twitterInfo.py
@@ -1,6 +1,7 @@
 import twitter
+import argparse
 
-#Create twitter API
+#Create twitter API with authentication from Beatrice's profile
 api = twitter.Api(
 consumer_key="grfzFcIHU6kiS3xshGUf6Q",
 consumer_secret="KKqYXiNCWMuLzIlmx8OLkXG4i3Fsn8i7SNIKC71SOQ",
@@ -11,14 +12,35 @@ access_token_secret="WTokWyRynBMa8FIWIHWWiGJR2bYgPWYcwkH6IowtYRtoy")
 authorList = open('authors.txt', 'r').readlines()
 authorList = map(lambda s: s.strip(), authorList)
 
-#Write header
-outputFile = open('Author_Info.txt', 'w')
-outputFile.write("Twitter name, real name, followers, following, tweets\n")
+#Handle command line argument
+parser = argparse.ArgumentParser()
+parser.add_argument("type", help="What type of data to crunch", choices=["info", "followers"])
+parser.add_argument("-o", "--output", help="Output file", default="output.txt")
+args = parser.parse_args()
 
-for author in authorList:
-	print author
-	usr = api.GetUser(screen_name=author)
-	output = author + ', ' + usr.GetName() + ', ' + str(usr.GetFollowersCount()) + ', ' + str(usr.GetFriendsCount()) + ', ' + str(usr.GetStatusesCount()) + '\n'
-	outputFile.write(output.encode('utf8'))
+outputFile = open(args.output, 'w')
 
+###########################
+#Handle info style output
+
+if args.type == "info":
+	#Header
+	outputFile.write("Twitter name, real name, followers, following, tweets\n")
+	#Content
+	for author in authorList:
+		print author
+		usr = api.GetUser(screen_name=author)
+		output = author + ', ' + usr.GetName() + ', ' + str(usr.GetFollowersCount()) + ', ' + str(usr.GetFriendsCount()) + ', ' + str(usr.GetStatusesCount()) + '\n'
+		outputFile.write(output.encode('utf8'))
+
+############################
+#Handle followers style output
+
+if args.type == "followers":
+	print "Followers output"
+	outputFile.write("Test\n")
+
+
+
+#Cleaning
 outputFile.close()
