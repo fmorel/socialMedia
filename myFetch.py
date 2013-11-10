a7a3286d1965df23e53e219fe9bdd13a934e9f5a
François-Xavier MOREL <efix.morel@gmail.com>
2013-11-10 13:49:34 +0100

    Parse input from external list and write CSV output

--- myFetch.py
+++ myFetch.py
@@ -3,18 +3,30 @@ import urllib2
 
 #example query
 #  - http://otter.topsy.com/search.json?q=m-commerce%20from%3Atcadierno&apikey=09C43A9B270A470B8EB8F2946A9369F3&window=d30
-#Fetch some URL
 
+#Extract lists from input files
+authorList = open('authors.txt', 'r').readlines()
+keywordList = open('keywords.txt', 'r').readlines()
 
+authorList = map(lambda s: s.strip(), authorList)
+keywordList = map(lambda s: s.strip(), keywordList)
 
-keyword = 'mobile'
-author='tcadierno'
-myQuery = 'http://otter.topsy.com/search.json?q={}%20from%3A{}&apikey=09C43A9B270A470B8EB8F2946A9369F3&window=d180'.format(keyword, author)
-response = urllib2.urlopen(myQuery)
-#print response.info()
-json = json.loads(response.read())
-# do something
-total = json['response']['total']
-print "Total tweets : "
-print total
-response.close()  # best practice to close the file
+#Build Header line
+output='Author'
+for keyword in keywordList:
+	output = output+ ', ' + keyword
+
+#Build content
+for author in authorList:
+	output = output + '\n' + author
+	for keyword in keywordList:
+		print author + ' ' + keyword
+		myQuery = 'http://otter.topsy.com/search.json?q={}%20from%3A{}&apikey=09C43A9B270A470B8EB8F2946A9369F3&window=d180'.format(keyword, author)
+		response = urllib2.urlopen(myQuery)
+		jsonData = json.loads(response.read())
+		total = jsonData['response']['total']
+		output = output + ', ' + str(total)
+		response.close()  # best practice to close the file
+
+open ('Author_Keywords.txt', 'w').write(output)
+
